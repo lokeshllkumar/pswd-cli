ff873c6356c580b082dbaa5aca141af309fecb98
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-05-28 11:23:46 +0530

    created a subcommand to list all stored password records in the database

---
+++ cmd/list.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"encoding/json"
+
+	"github.com/lokeshllkumar/pswd-cli/models"
+	"github.com/lokeshllkumar/pswd-cli/utils"
+	"github.com/spf13/cobra"
+)
+
+var ListCmd = &cobra.Command{
+	Use:   "List",
+	Short: "The 'list' subcommand will display all stored password records that exist in the database",
+	Run: func(cmd *cobra.Command, args []string) {
+		db, err := models.InitDB()
+		if err != nil {
+			fmt.Println("Error, database connection failed")
+			return
+		}
+
+		data, err := db.ListPasswords()
+		if err != nil {
+			fmt.Println("Error, data could not be fetched")
+			return
+		}
+
+		var res []models.PasswordEntry
+		if err := json.Unmarshal([]byte(data), &res); err != nil {
+			log.Fatal("Error retrieving data")
+		}
+
+		for _, record := range res {
+			decryptedPassword, err := utils.DecryptPassword(record.Password)
+			if err != nil {
+				fmt.Println("Error decrypting stored password")
+				continue
+			}
+
+			fmt.Printf("Service: %s |\tUsername: %s |\tPassword: %s |\tCreated At: %s\n", record.Service, record.Username, decryptedPassword, record.TimeOfCreation.Format(time.RFC3339))
+		}
+
+		db.CloseDB()
+	},
+}
